44e24157347c819fdbbd291d2c1833383fe7de15
Jan Pfeifer <pfeifer@gmail.com>
2026-04-26 08:45:01 +0200

    Moved exec tests to generic `backendtest`

--- support/backendtest/exec.go
+++ support/backendtest/exec.go
@@ -3,6 +3,7 @@
 package backendtest
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/gomlx/compute"
@@ -111,31 +112,76 @@ func TestExec(t *testing.T, b compute.Backend) {
 
 	t.Run("BufferReuse", func(t *testing.T) {
 		testutil.SkipIfMissing(t, b, compute.OpTypeNeg)
+		testutil.SkipIfMissing(t, b, compute.OpTypeConstant)
+		if !b.HasSharedBuffers() {
+			t.Skipf("Backend does not support shared buffers, we can't actually test the reuse semantics, skipping")
+		}
+
+		shape := shapes.Make(dtypes.Float32, 10, 1000, 1000)
 		builder := b.Builder("test_reuse")
 		mainFn := builder.Main()
-		x, _ := mainFn.Parameter("x", shapes.Make(dtypes.Float32, 3), nil)
+		x, _ := mainFn.Parameter("x", shape, nil)
 		negX, _ := mainFn.Neg(x)
-		_ = mainFn.Return([]compute.Value{negX}, nil)
+		c, _ := mainFn.Constant([]int64{1, 2, 3}, 3)
+		_ = mainFn.Return([]compute.Value{negX, c}, nil)
 		exec, _ := builder.Compile()
 
-		// Checks correct execution with donated inputs.
-		i0, _ := b.BufferFromFlatData(0, []float32{3, 5, 7}, shapes.Make(dtypes.Float32, 3))
+		// Checks correct execution with donated inputs, and that the output reused the input buffer.
+		i0, i0Flat, err := b.NewSharedBuffer(0, shape)
+		if err != nil {
+			t.Fatalf("NewSharedBuffer failed: %+v", err)
+		}
+		i0Pointer := reflect.ValueOf(i0Flat).Pointer()
+
 		outputs, err := exec.Execute([]compute.Buffer{i0}, []bool{true}, 0)
 		if err != nil {
-			t.Fatalf("unexpected error: %+v", err)
+			t.Fatalf("Execute failed: %+v", err)
 		}
-		if len(outputs) != 1 {
-			t.Fatalf("expected 1 output, got %d", len(outputs))
+		if len(outputs) != 2 {
+			t.Fatalf("expected 2 outputs, got %d", len(outputs))
+		}
+
+		// i0 should already have been finalized/donated, so this shoudl be a no-op.
+		err = b.BufferFinalize(i0)
+		if err != nil {
+			t.Fatalf("BufferFinalize failed: %+v", err)
+		}
+
+		// Verify reuse via pointer comparison of underlying shared data.
+		out0Flat, err := b.BufferData(outputs[0])
+		if err != nil {
+			t.Fatalf("BufferData failed: %+v", err)
+		}
+		out0Pointer := reflect.ValueOf(out0Flat).Pointer()
+		if i0Pointer != out0Pointer {
+			if b.Name() == "go" {
+				t.Errorf("Expected 'go' backend to reuse the underlying data pointer of the donated input buffer")
+			} else {
+				t.Logf("Note: output data pointer (0x%X) did not reuse the donated input data pointer (0x%X). "+
+					"This is ok as there is no gauranteed sharing the exact same memory for the donated buffers.",
+					out0Pointer, i0Pointer)
+			}
+		}
+
+		outputShape, err := b.BufferShape(outputs[1])
+		if err != nil {
+			t.Fatalf("BufferShape failed: %+v", err)
+		}
+		if !outputShape.Equal(shapes.Make(dtypes.Int64, 3)) {
+			t.Errorf("Expected output shape %s, got %s", shapes.Make(dtypes.Int64, 3), outputShape)
 		}
 
 		// Checks correct execution without donated inputs.
-		i1, _ := b.BufferFromFlatData(0, []float32{3, 5, 7}, shapes.Make(dtypes.Float32, 3))
+		i1, i1Flat, _ := b.NewSharedBuffer(0, shape)
+		i1Pointer := reflect.ValueOf(i1Flat).Pointer()
 		outputs, err = exec.Execute([]compute.Buffer{i1}, []bool{false}, 0)
 		if err != nil {
-			t.Fatalf("unexpected error: %+v", err)
+			t.Fatalf("Execute failed: %+v", err)
 		}
-		if len(outputs) != 1 {
-			t.Fatalf("expected 1 output, got %d", len(outputs))
+		out0FlatNoDonate, _ := b.BufferData(outputs[0])
+		out0PointerNoDonate := reflect.ValueOf(out0FlatNoDonate).Pointer()
+		if i1Pointer == out0PointerNoDonate {
+			t.Errorf("Expected output data buffer to be different from input data buffer when input buffer not donated")
 		}
 	})
 }
