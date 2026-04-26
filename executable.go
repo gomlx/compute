ac563920357be0a91fad477f12c032c3c5d393cd
Jan Pfeifer <pfeifer@gmail.com>
2026-04-26 15:17:09 +0200

    Updated Executable.Execute comment.

--- executable.go
+++ executable.go
@@ -18,7 +18,7 @@ type Executable interface {
 	Outputs() (outputShapes []shapes.Shape)
 
 	// Execute the computation.
-	// The number and shapes of the inputs must match those returned by Inputs.
+	// The number and shapes of the inputs must match those of the Executable parameters (returned by [Executable.Inputs]).
 	//
 	// The inputs marked as donate will become invalid after use.
 	// This is useful if the input buffer is no longer needed or if updating a variable
@@ -29,5 +29,9 @@ type Executable interface {
 	//
 	// For portable computations (not compiled with a fixed device assignment), the execution runs on the defaultDevice.
 	// For non-portable computations (where the device assignment is fixed), the defaultDevice is ignored.
+	//
+	// For SPMD distributed computations (see [Builder.DistributedSPMD]), the executable is replicated on each device.
+	// There will be multiple inputs per executable parameter, one per device.
+	// They are organized as "device-major", that is the input for parameter i on device j is given by inputs[j*numParams + i].
 	Execute(inputs []Buffer, donate []bool, defaultDevice DeviceNum) ([]Buffer, error)
 }
