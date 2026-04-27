e4acbfd4e87ea03a3254c8cc3ff7198e1ff16d1f
Jan Pfeifer <pfeifer@gmail.com>
2026-04-27 09:11:22 +0200

    Removed gomlx tensors dependency.

--- go.mod
+++ go.mod
@@ -3,14 +3,10 @@ module github.com/gomlx/compute
 go 1.26
 
 require (
-	github.com/gomlx/gomlx v0.27.3
 	github.com/google/go-cmp v0.7.0
 	github.com/pkg/errors v0.9.1
 	golang.org/x/exp v0.0.0-20260410095643-746e56fc9e2f
 	k8s.io/klog/v2 v2.140.0
 )
 
-require (
-	github.com/go-logr/logr v1.4.3 // indirect
-	github.com/x448/float16 v0.8.4 // indirect
-)
+require github.com/go-logr/logr v1.4.3 // indirect
--- support/testutil/testutil.go
+++ support/testutil/testutil.go
@@ -13,12 +13,10 @@
 package testutil
 
 import (
-	"fmt"
 	"math"
 
 	"github.com/gomlx/compute/dtypes/bfloat16"
 	"github.com/gomlx/compute/dtypes/float16"
-	"github.com/gomlx/gomlx/pkg/core/tensors"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -53,20 +51,6 @@ func withinDeltaHalfPrecision[T halfFloat](a, b T, delta float64) bool {
 // IsInDelta reports if want and got are equal within a given absolute delta.
 // If they are not equal, it returns the diff using the format "-want +got").
 func IsInDelta(want, got any, delta float64) (ok bool, diff string) {
-	if tA, okA := want.(*tensors.Tensor); okA {
-		// Special case tensors:
-		tB, okB := got.(*tensors.Tensor)
-		if !okB {
-			return false, fmt.Sprintf("values have different types: %T and %T", want, got)
-		}
-		tA.ConstFlatData(func(tAFlat any) {
-			tB.ConstFlatData(func(tBFlat any) {
-				ok, diff = IsInDelta(tAFlat, tBFlat, delta)
-			})
-		})
-		return ok, diff
-	}
-
 	opts := []cmp.Option{
 		cmp.Comparer(func(a, b float32) bool { return withinDeltaBase(a, b, delta) }),
 		cmp.Comparer(func(a, b float64) bool { return withinDeltaBase(a, b, delta) }),
@@ -92,20 +76,6 @@ func withinRelativeDeltaHalfPrecision[T halfFloat](a, b T, relDelta float64) boo
 // IsInRelativeDelta reports if want and got are equal within a given relative delta.
 // If they are not equal, it returns the diff using the format "-want +got").
 func IsInRelativeDelta(want, got any, relDelta float64) (ok bool, diff string) {
-	if tA, okA := want.(*tensors.Tensor); okA {
-		// Special case tensors:
-		tB, okB := got.(*tensors.Tensor)
-		if !okB {
-			return false, fmt.Sprintf("values have different types: %T and %T", want, got)
-		}
-		tA.ConstFlatData(func(tAFlat any) {
-			tB.ConstFlatData(func(tBFlat any) {
-				ok, diff = IsInRelativeDelta(tAFlat, tBFlat, relDelta)
-			})
-		})
-		return ok, diff
-	}
-
 	opts := []cmp.Option{
 		cmp.Comparer(func(a, b float32) bool { return withinRelativeDeltaBase(a, b, relDelta) }),
 		cmp.Comparer(func(a, b float64) bool { return withinRelativeDeltaBase(a, b, relDelta) }),
@@ -133,19 +103,6 @@ func Try(f func()) (panicked bool, reason any) {
 // IsEqual returns whether want and got are equal using go-cmp.
 // If they are not equal, it returns the diff using the format "-want +got").
 func IsEqual(want, got any) (ok bool, diff string) {
-	if tA, ok := want.(*tensors.Tensor); ok {
-		tB, ok := got.(*tensors.Tensor)
-		if !ok {
-			return false, fmt.Sprintf("values have different types: %T and %T", want, got)
-		}
-		tA.ConstFlatData(func(tAFlat any) {
-			tB.ConstFlatData(func(tBFlat any) {
-				ok, diff = IsEqual(tAFlat, tBFlat)
-			})
-		})
-		return ok, diff
-	}
-
 	if cmp.Equal(want, got) {
 		return true, ""
 	}
