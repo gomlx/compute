11a185afb0346a6fd9b9823daaa383a2e15c17de
Jan Pfeifer <pfeifer@gmail.com>
2026-04-28 07:44:32 +0200

    Updated comments.

--- internal/cmd/notimplemented_generator/main.go
+++ internal/cmd/notimplemented_generator/main.go
@@ -1,11 +1,6 @@
 // Copyright 2023-2026 The GoMLX Authors. SPDX-License-Identifier: Apache-2.0
 
-// backends_generator generates parts of the backends.Builder interface based on the github.com/gomlx/gopjrt/xlabuilder implementation.
-//
-// Although GoMLX can support more than one backend, the XlaBuilder is the reference implementation for now.
-//
-// If the environment variable GOPJRT_SRC is set, it parses the ops from there.
-// Otherwise it clones the gopjrt repository to a temporary sub-directory.
+// notimplemented_generator generates "notimplemented" stubs for every API of the compute.Backend interface
 package main
 
 import (
