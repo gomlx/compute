98060bf70bcd410547961794de13c5581604d9ec
Jan Pfeifer <pfeifer@gmail.com>
2026-05-02 10:37:10 +0200

    Fixed IsNaN op.

--- internal/gobackend/ops/isnan.go
+++ internal/gobackend/ops/isnan.go
@@ -1,7 +1,18 @@
 package ops
 
+import (
+	"github.com/gomlx/compute"
+	"github.com/gomlx/compute/internal/gobackend"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	gobackend.RegisterIsNaN.Register(IsNaN, gobackend.PriorityGeneric)
+}
+
 // IsNaN implements compute.Builder interface.
-func (f *Function) IsNaN(x compute.Value) (compute.Value, error) {
+// Notice there is no executor because it uses f.NotEqual instead.
+func IsNaN(f *gobackend.Function, x compute.Value) (compute.Value, error) {
 	result, err := f.NotEqual(x, x)
 	if err != nil {
 		return nil, errors.WithMessage(err, "while building op IsNaN")
