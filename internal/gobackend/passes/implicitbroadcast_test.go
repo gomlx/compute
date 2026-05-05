9de049f9e94355c38c5c102fee0ac024852235e4
Jan Pfeifer <pfeifer@gmail.com>
2026-05-05 13:58:01 +0200

    Added test with binaryOps where both inputs are BroadcastInDims.

--- internal/gobackend/passes/implicitbroadcast_test.go
+++ internal/gobackend/passes/implicitbroadcast_test.go
@@ -110,4 +110,77 @@ func TestImplicitBroadcastFusion(t *testing.T) {
 			t.Errorf("Incorrect result:\n%s", diff)
 		}
 	})
+
+	t.Run("BothInputsBroadcast", func(t *testing.T) {
+		builder := backend.Builder("BothInputsBroadcast").(*gobackend.Builder)
+		main := builder.Main().(*gobackend.Function)
+
+		// lhs: [1, 1, 1], Broadcasted to [2, 1, 5]
+		x, _ := main.Parameter("x", shapes.Make(dtypes.Float32, 1, 1, 1), nil)
+		xBroadcast, _ := main.BroadcastInDim(x, shapes.Make(dtypes.Float32, 2, 1, 5), []int{0, 1, 2})
+
+		// rhs: [2, 1, 1], Broadcasted to [2, 3, 1]
+		y, _ := main.Parameter("y", shapes.Make(dtypes.Float32, 2, 1, 1), nil)
+		yBroadcast, _ := main.BroadcastInDim(y, shapes.Make(dtypes.Float32, 2, 3, 1), []int{0, 1, 2})
+
+		// Binary op: Add [2, 1, 5] + [2, 3, 1] -> Implicit broadcast to [2, 3, 5]
+		z, _ := main.Add(xBroadcast, yBroadcast)
+
+		// Add another node to check that the graph is properly re-sorted after optimization.
+		zSqrt, err := main.Sqrt(z)
+		if err != nil {
+			t.Fatalf("Failed to create Sqrt node: %+v", err)
+		}
+
+		_ = main.Return([]compute.Value{zSqrt}, nil)
+
+		zNode := z.(*gobackend.Node)
+		// Before compilation, inputs are BroadcastInDim.
+		if zNode.Inputs[0].OpType != compute.OpTypeBroadcastInDim || zNode.Inputs[1].OpType != compute.OpTypeBroadcastInDim {
+			t.Fatalf("Expected both inputs of Add to be BroadcastInDim before optimization")
+		}
+
+		// Compile triggers Optimization.
+		exec, err := builder.Compile()
+		if err != nil {
+			t.Fatalf("Failed to compile: %+v", err)
+		}
+
+		// Verify that z's input was fused to x and y.
+		if zNode.Inputs[0].OpType == compute.OpTypeBroadcastInDim || zNode.Inputs[1].OpType == compute.OpTypeBroadcastInDim {
+			t.Errorf("BroadcastInDim should have been fused out")
+		}
+		if zNode.Inputs[0] != x.(*gobackend.Node) || zNode.Inputs[1] != y.(*gobackend.Node) {
+			t.Errorf("Expected inputs of Add to be x and y")
+		}
+
+		// Execute to ensure correctness
+		xData := []float32{2}
+		yData := []float32{10, 20}
+		xBuf, _ := backend.BufferFromFlatData(0, xData, shapes.Make(dtypes.Float32, 1, 1, 1))
+		yBuf, _ := backend.BufferFromFlatData(0, yData, shapes.Make(dtypes.Float32, 2, 1, 1))
+		outputs, err := exec.Execute([]compute.Buffer{xBuf, yBuf}, nil, 0)
+		if err != nil {
+			t.Fatalf("Execute failed: %+v", err)
+		}
+
+		resultBuf := outputs[0].(*gobackend.Buffer)
+		shape, _ := resultBuf.Shape()
+		if !shape.Equal(shapes.Make(dtypes.Float32, 2, 3, 5)) {
+			t.Errorf("Incorrect result shape: %s", shape)
+		}
+
+		result := resultBuf.Flat.([]float32)
+		expected := make([]float32, 30)
+		for i := 0; i < 15; i++ {
+			expected[i] = 12
+		}
+		for i := 15; i < 30; i++ {
+			expected[i] = 22
+		}
+
+		if ok, diff := testutil.IsEqual(expected, result); !ok {
+			t.Errorf("Incorrect result:\n%s", diff)
+		}
+	})
 }
