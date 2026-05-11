6a57f2e5d387bcea90fa3579d41ed98df88daf70
Jan Pfeifer <pfeifer@gmail.com>
2026-05-11 08:59:10 +0200

    Added AVX2 support for float16 and bfloat16 packages.

---
+++ dtypes/float16/avx2.go
@@ -0,0 +1,81 @@
+// Copyright 2023-2026 The GoMLX Authors. SPDX-License-Identifier: Apache-2.0
+
+//go:build amd64 && goexperiment.simd
+
+package float16
+
+import (
+	"simd/archsimd"
+	"unsafe"
+)
+
+// LoadFloat16x16 loads 16 Float16 values from the given pointer into a Float16x16 vector.
+//
+// The pointer must be aligned to 32 bytes, just like usual AVX2 loads.
+//
+// Only available if archsimd.X86.AVX2() returns true at runtime -- indicating AVX2 is available.
+// If not available, don't use this, it will crash!
+func LoadFloat16x16(ptr *[16]Float16) Float16x16 {
+	vec := archsimd.LoadUint16x16((*[16]uint16)(unsafe.Pointer(ptr)))
+	return Float16x16(vec)
+}
+
+// Float16x16 is an alias for archsimd.Uint16x16 with corresponding utility methods.
+//
+// Only available if archsimd.X86.AVX2() returns true at runtime -- indicating AVX2 is available.
+// If not available, don't use this, it will crash!
+type Float16x16 archsimd.Uint16x16
+
+// ToFloat32 converts a vector of 16 float16 values (as an archsimd.Uint16x16) to two vectors of 8 float32 values
+// (as archsimd.Float32x8).
+func (v Float16x16) ToFloat32() (lo, hi archsimd.Float32x8) {
+	// Cast back to use the archsimd methods
+	vec := archsimd.Uint16x16(v)
+	lo32 := convertF16ToF32AVX2(vec.GetLo().ExtendToUint32())
+	hi32 := convertF16ToF32AVX2(vec.GetHi().ExtendToUint32())
+	return lo32.AsFloat32x8(), hi32.AsFloat32x8()
+}
+
+func convertF16ToF32AVX2(v archsimd.Uint32x8) archsimd.Uint32x8 {
+	sign := v.And(archsimd.BroadcastUint32x8(0x8000)).ShiftAllLeft(16)
+	exp := v.And(archsimd.BroadcastUint32x8(0x7C00)).ShiftAllRight(10)
+	mantissa := v.And(archsimd.BroadcastUint32x8(0x03FF))
+
+	isZero := exp.Equal(archsimd.BroadcastUint32x8(0)).And(mantissa.Equal(archsimd.BroadcastUint32x8(0)))
+	isDenorm := exp.Equal(archsimd.BroadcastUint32x8(0)).And(mantissa.Greater(archsimd.BroadcastUint32x8(0)))
+	isInfNaN := exp.Equal(archsimd.BroadcastUint32x8(0x1F))
+
+	// Case: Normalized (Default)
+	// exp32 = (exp - 15 + 127) << 23 = (exp + 112) << 23
+	normExp32 := exp.Add(archsimd.BroadcastUint32x8(112)).ShiftAllLeft(23)
+	normMant32 := mantissa.ShiftAllLeft(13)
+	normRes := sign.Or(normExp32).Or(normMant32)
+
+	// Case: Inf/NaN
+	// exp32 = 0xFF << 23 = 0x7F800000
+	infNanRes := sign.Or(archsimd.BroadcastUint32x8(0x7F800000)).Or(normMant32)
+
+	// Case: Zero
+	zeroRes := sign
+
+	// Case: Denormal
+	lz := mantissa.LeadingZeros()
+	shift := lz.Sub(archsimd.BroadcastUint32x8(21))
+	// exp32 = (1 - shift - 15 + 127) << 23 = (113 - shift) << 23
+	denormExp32 := archsimd.BroadcastUint32x8(113).Sub(shift).ShiftAllLeft(23)
+	// mantissa32 = ((mantissa << shift) & 0x03FF) << 13
+	denormMant32 := mantissa.ShiftLeft(shift).And(archsimd.BroadcastUint32x8(0x03FF)).ShiftAllLeft(13)
+	denormRes := sign.Or(denormExp32).Or(denormMant32)
+
+	// Combine
+	isZeroBits := isZero.ToInt32x8().AsUint32x8()
+	isDenormBits := isDenorm.ToInt32x8().AsUint32x8()
+	isInfNaNBits := isInfNaN.ToInt32x8().AsUint32x8()
+	isSpecialBits := isZeroBits.Or(isDenormBits).Or(isInfNaNBits)
+
+	res := normRes.AndNot(isSpecialBits)
+	res = res.Or(infNanRes.And(isInfNaNBits))
+	res = res.Or(zeroRes.And(isZeroBits))
+	res = res.Or(denormRes.And(isDenormBits))
+	return res
+}
