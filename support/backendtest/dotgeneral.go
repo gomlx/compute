7ed1eb1db94841e15603ba11641aa8b1dc4d8146
Jan Pfeifer <pfeifer@gmail.com>
2026-06-21 20:24:31 +0200

    Relaxed the test of large dot product with transpose -- it was displaying some small numeric differences due to XLA CUDA using TF32 by default.

--- support/backendtest/dotgeneral.go
+++ support/backendtest/dotgeneral.go
@@ -535,9 +535,9 @@ func TestDotGeneral(t *testing.T, backend compute.Backend) {
 			t.Fatalf("testutil.Exec1 failed: %v", err)
 		}
 
-		// We use a relatively high delta (0.3) to allow the test to pass with the "xla:cuda,tf32" backend,
+		// We use a relatively high delta (0.5) to allow the test to pass with the "xla:cuda,tf32" backend,
 		// which uses TF32 precision (10-bit mantissa) and is therefore less precise than full float32.
-		delta := 0.3
+		delta := 0.5
 		if ok, diff := testutil.IsInDelta(wantFlat, gotFlat, delta); !ok {
 			t.Fatalf("Unexpected result (-want +got):\n%s", diff)
 		}
