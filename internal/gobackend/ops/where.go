791e442468e002d43f8939bafc2be741383e57bc
Jan Pfeifer <pfeifer@gmail.com>
2026-09-10 14:20:24 +0200

    Chunked and parallelized Where.

--- internal/gobackend/ops/where.go
+++ internal/gobackend/ops/where.go
@@ -1,7 +1,12 @@
 package ops
 
 import (
+	"sync"
+
 	"github.com/gomlx/compute"
+	"github.com/gomlx/compute/dtypes"
+	"github.com/gomlx/compute/dtypes/bfloat16"
+	"github.com/gomlx/compute/dtypes/float16"
 	"github.com/gomlx/compute/internal/gobackend"
 	"github.com/gomlx/compute/shapeinference"
 )
@@ -50,6 +55,11 @@ func execWhere(backend *gobackend.Backend, node *gobackend.Node, inputs []*gobac
 	if backend.NoOps {
 		return output, nil
 	}
+
+	if dispatchWhereParallel(backend, condition, onTrue, onFalse, output) {
+		return output, nil
+	}
+
 	tmpAny, tmpErr := whereDTypeMap.Get(outputShape.DType)
 	if tmpErr != nil {
 		panic(tmpErr)
@@ -57,6 +67,111 @@ func execWhere(backend *gobackend.Backend, node *gobackend.Node, inputs []*gobac
 	fn := tmpAny.(func(conditionBuf, onTrueBuf, onFalseBuf, outputBuf *gobackend.Buffer))
 	fn(condition, onTrue, onFalse, output)
 	return output, nil
+}
+
+func dispatchWhereParallel(backend *gobackend.Backend, conditionBuf, onTrueBuf, onFalseBuf, outputBuf *gobackend.Buffer) bool {
+	if conditionBuf.RawShape.IsScalar() {
+		return false
+	}
+	n := conditionBuf.RawShape.Size()
+	if backend == nil || backend.Workers == nil || !backend.Workers.IsEnabled() || n <= 32768 {
+		return false
+	}
+	cond := conditionBuf.Flat.([]bool)
+	switch outputBuf.RawShape.DType {
+	case dtypes.Float32:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]float32))
+	case dtypes.Float64:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]float64))
+	case dtypes.BFloat16:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]bfloat16.BFloat16))
+	case dtypes.Float16:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]float16.Float16))
+	case dtypes.Int32:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]int32))
+	case dtypes.Int64:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]int64))
+	case dtypes.Int16:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]int16))
+	case dtypes.Int8:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]int8))
+	case dtypes.Uint32:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]uint32))
+	case dtypes.Uint64:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]uint64))
+	case dtypes.Uint16:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]uint16))
+	case dtypes.Uint8:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]uint8))
+	case dtypes.Bool:
+		parallelWhere(backend, cond, onTrueBuf, onFalseBuf, outputBuf.Flat.([]bool))
+	default:
+		return false
+	}
+	return true
+}
+
+func parallelWhere[T any](backend *gobackend.Backend, cond []bool, onTrueBuf, onFalseBuf *gobackend.Buffer, out []T) {
+	n := len(cond)
+	onTrueIsScalar := onTrueBuf.RawShape.IsScalar()
+	onFalseIsScalar := onFalseBuf.RawShape.IsScalar()
+	onTrueFlat := onTrueBuf.Flat.([]T)
+	onFalseFlat := onFalseBuf.Flat.([]T)
+	var onTrueScalar, onFalseScalar T
+	if onTrueIsScalar {
+		onTrueScalar = onTrueFlat[0]
+	}
+	if onFalseIsScalar {
+		onFalseScalar = onFalseFlat[0]
+	}
+
+	numWorkers := backend.Workers.AdjustedMaxParallelism()
+	targetChunks := min(n, max(1, numWorkers*2))
+	chunkSize := max(16384, (n+targetChunks-1)/targetChunks)
+	var wg sync.WaitGroup
+
+	for start := 0; start < n; start += chunkSize {
+		end := min(start+chunkSize, n)
+		wg.Add(1)
+		backend.Workers.WaitToStart(func() {
+			switch {
+			case !onTrueIsScalar && onFalseIsScalar:
+				for i := start; i < end; i++ {
+					if cond[i] {
+						out[i] = onTrueFlat[i]
+					} else {
+						out[i] = onFalseScalar
+					}
+				}
+			case onTrueIsScalar && !onFalseIsScalar:
+				for i := start; i < end; i++ {
+					if cond[i] {
+						out[i] = onTrueScalar
+					} else {
+						out[i] = onFalseFlat[i]
+					}
+				}
+			case !onTrueIsScalar && !onFalseIsScalar:
+				for i := start; i < end; i++ {
+					if cond[i] {
+						out[i] = onTrueFlat[i]
+					} else {
+						out[i] = onFalseFlat[i]
+					}
+				}
+			default:
+				for i := start; i < end; i++ {
+					if cond[i] {
+						out[i] = onTrueScalar
+					} else {
+						out[i] = onFalseScalar
+					}
+				}
+			}
+			wg.Done()
+		})
+	}
+	wg.Wait()
 }
 
 //gobackend:dtypemap execWhereGeneric ints,uints,floats,half,bool
